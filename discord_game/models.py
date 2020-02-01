e71bb1033e52ab13d67abb3c21fc618d6a6731f9
Bruno L. Carli <brunolcarli@gmail.com>
2020-02-01 15:38:46 -0300

    ADD profile and item models

--- discord_game/models.py
+++ discord_game/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Define as caracteristicas de um usuário do sistema.
+    """
+    discord_id = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        unique=True
+    )
+    exp_earned = models.IntegerField(default=0)
+
+
+class Item(models.Model):
+    """
+    Define as caracteristicas de um item do jogo.
+    """
+    name = models.CharField(max_length=30, null=False, blank=False, unique=True)
+    description = models.TextField(blank=True, null=True)
