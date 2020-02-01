96bbfc869f998537625346c2df603469becbe5a5
Bruno L. Carli <brunolcarli@gmail.com>
2020-02-01 15:33:59 -0300

    ADD api version query

--- discord_game/schema.py
+++ discord_game/schema.py
@@ -1,10 +1,11 @@
 import graphene
+from kessel.settings.common import __version__
 
 
 class Query(object):
-    foo = graphene.String()
-    def resolve_foo(self, info, **kwargs):
-        return "Foo"
+    api_version = graphene.String()
+    def resolve_api_version(self, info, **kwargs):
+        return __version__
 
 
 class Mutation(object):
